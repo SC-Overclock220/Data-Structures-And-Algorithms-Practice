569690fa7e66006aae876ee519660a0fe8d3a22c
Sagnik Chakraborty <contact.sagnikchakraborty2@gmail.com>
2025-04-26 08:38:33 +0530

    Linked List Implemented Successfully

--- DSA in Javascript/Linked List/Singly Linked List/Main.js
+++ DSA in Javascript/Linked List/Singly Linked List/Main.js
@@ -7,6 +7,20 @@ sll1.addToTail(3);
 sll1.addToTail(6);
 sll1.addToTail(8);
 sll1.addToHead(5);
+sll1.addToPos(9, 4);
 
+sll1.printLinkedList();
+sll1.continuityCheck();
+
+sll1.addToPos(7, 2);
+sll1.printLinkedList();
+sll1.addToPos(20, 6);
+sll1.printLinkedList();
+sll1.continuityCheck();
+
+sll1.deleteFromPos(4);
+sll1.printLinkedList();
+sll1.continuityCheck();
+sll1.deleteFromPos(7);
 sll1.printLinkedList();
 sll1.continuityCheck();
--- DSA in Javascript/Linked List/Singly Linked List/SinglyLinkedList.js
+++ DSA in Javascript/Linked List/Singly Linked List/SinglyLinkedList.js
@@ -46,6 +46,34 @@ export class SinglyLinkedList {
     console.log("Node Inserted At Tail");
   }
 
+  addToPos(value, pos) {
+    if (!this.#head) {
+      this.createLinkedList(value);
+      return;
+    }
+
+    if (pos == 1) {
+      this.addToHead(value);
+      return;
+    }
+    if (pos > this.#sizeOfLL) {
+      this.addToTail(value);
+      return;
+    }
+    let newNode = new ListNode(value);
+    let temp = this.#head;
+
+    for (let i = 1; i < pos - 1; i++) {
+      temp = temp.next;
+    }
+
+    let nextNode = temp.next;
+    temp.next = newNode;
+    newNode.next = nextNode;
+    this.#sizeOfLL++;
+    console.log("Node Inserted At Position " + pos);
+  }
+
   printLinkedList() {
     let temp = this.#head;
 
@@ -56,7 +84,65 @@ export class SinglyLinkedList {
     process.stdout.write("NULL \n");
   }
 
+  deleteFromHead() {
+    if (!this.#head) {
+      console.log("Nothing To Delete! Linked List Is Empty");
+      return;
+    }
+
+    this.#head = this.#head.next;
+    this.#sizeOfLL--;
+    console.log("Node Deleted From Head");
+  }
+
+  deleteFromTail() {
+    if (!this.#head) {
+      console.log("Nothing To Delete! Linked List Is Empty");
+      return;
+    }
+
+    let temp = this.#head;
+
+    while (temp.next !== tail) temp = temp.next;
+
+    this.#head = this.#head.next;
+    this.#sizeOfLL--;
+    console.log("Node Deleted From Head");
+  }
+
+  deleteFromPos(pos) {
+    if (!this.#head) {
+      console.log("Nothing To Delete! Linked List Is Empty");
+      return;
+    }
+
+    if (pos <= 0) {
+      console.log(`Invalid Position. Cannot Perform Delete`);
+      return;
+    }
+
+    if (pos === 0) {
+      this.deleteFromHead();
+      return;
+    }
+
+    if (pos >= this.#sizeOfLL) {
+      this.deleteFromTail();
+      return;
+    }
+
+    let temp = this.#head;
+
+    for (let i = 1; i < pos - 1; i++) {
+      temp = temp.next;
+    }
+
+    temp.next = temp.next.next;
+    this.#sizeOfLL--;
+    console.log(`Node Deleted From Position ${pos}`);
+  }
+
   continuityCheck() {
-    console.log(this.#tail, this.#tail.next, this.#head);
+    console.log(this.#tail, this.#tail.next, this.#head, this.#sizeOfLL);
   }
 }
